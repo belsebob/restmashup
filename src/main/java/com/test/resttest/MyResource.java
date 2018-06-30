ceae1221b21541f865ec766009f52085342b6247
belsebob <marnils@gmail.com>
2018-06-30 20:22:40 +0200

    Add files via upload

---
+++ src/main/java/com/test/resttest/MyResource.java
@@ -0,0 +1,51 @@
+package com.test.resttest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import com.musicinfo.rest.InfoConsumer;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to
+	 * the client as "text/plain" media type.
+	 *
+	 * @return String that will be returned as a text/plain response.
+	 */
+
+	InfoConsumer infoconsumer = new InfoConsumer();
+
+	@GET
+	@Path("{MBID}")
+	public Response getUserById(@PathParam("MBID") String MBID) {
+
+//		return Response.status(200).entity("getUserById is called, id : " + id).build();
+		return infoconsumer.GetData(MBID);
+//		return Response.status(200).entity(infoconsumer.GetData(MBID)).build();
+
+	}
+
+//	@GET
+//	public Response people() {
+//		return infoconsumer.GetData();
+//	}
+
+	//
+	// @GET
+	// public JsonArray people() {
+	// return Json.createArrayBuilder().add(person("peter",
+	// 13)).add(person("pan", 67)).build();
+	// }
+	//
+	// public JsonObject person(String name, int age) {
+	// return Json.createObjectBuilder().add(name, age).build();
+	//
+	// }
+}
---
+++ src/main/java/com/test/resttest/heroku/Main.java
@@ -0,0 +1,26 @@
+package com.test.resttest.heroku;
+
+import javax.ws.rs.core.MediaType;
+
+import com.musicinfo.rest.InfoConsumer;
+import com.musicinfo.rest.InfoService;
+
+/**
+ * This class launches the web application in an embedded Jetty container. This is the entry point to your application. The Java
+ * command that is used for launching should fire this main method.
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+      InfoService infoService = new InfoService();
+    
+      
+      System.out.println("main");
+
+	
+      infoService.SetupServer();
+   
+      
+    }
+    
+}
