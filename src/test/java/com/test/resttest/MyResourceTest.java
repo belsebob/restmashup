6762c0e7caca1faa785a173d56759a9ef1436f2a
belsebob <marnils@gmail.com>
2018-07-01 09:52:19 +0200

    Not working

--- src/test/java/com/test/resttest/MyResourceTest.java
+++ src/test/java/com/test/resttest/MyResourceTest.java
@@ -21,9 +21,12 @@ public class MyResourceTest extends JerseyTest {
      * Test to see that the message "Got it!" is sent in the response.
      */
     @Test
-    public void testGetIt() {
-        final String responseMsg = target().path("myresource").request().get(String.class);
+    public void testGetArtistAlbumInfo() {
+        final String responseMsg = target().path("/5b11f4ce-a62d-471e-81fc-a69a8278c7").request().get(String.class);
+       
+        assert(responseMsg.contains("Nirvana"));        
+        assert(responseMsg.contains("In Bloom"));
+        assertEquals(true, responseMsg.contains("Nirvana"));    
 
-        assertEquals("Hello, Heroku!", responseMsg);
     }
 }
