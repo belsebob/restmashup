29174d9ff9c57340612fa1bf9f3207434911b5df
belsebob <marnils@gmail.com>
2018-07-03 18:09:18 +0200

    Not working

--- src/main/java/com/martin/restmashup/main/Main.java
+++ src/main/java/com/martin/restmashup/main/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
       RunServer runServer = new RunServer(); 
-      System.out.println("main");
       runServer.SetupServer();
    
     }
